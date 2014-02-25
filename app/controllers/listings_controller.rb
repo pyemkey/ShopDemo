e165d85297f3d47ce5f404a46506638aa4193b11
pyemkey <py.emkey@gmail.com>
2014-02-25 22:44:14 +0100

    add listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,25 @@
 class ListingsController < ApplicationController
 
 	def index
-		
+		@listings = Listing.all
 	end
 
+	def new
+		@listing = Listing.new                 
+	end
+
+	def create
+		@listing = Listing.new(secure_params)
+		if @listing.save
+			redirect_to root_path
+		else
+			render :new
+		end
+
+	end
+
+	private
+		def secure_params
+			params.require(:listing).permit(:name, :description, :price, :listing_image)
+		end
 end
---
+++ app/models/listing.rb
@@ -0,0 +1,12 @@
+class Listing < ActiveRecord::Base
+	has_attached_file :listing_image, 
+    styles: { medium: "200x200>", thumb: "100x100"},
+    storage: :dropbox,
+    dropbox_credentials: Rails.root.join("config/dropbox.yml")
+
+	validates_presence_of :name, :description, :price
+	validates :description, length: { maximum: 255 }
+	validates :price, numericality: true
+  validates_attachment_content_type :listing_image, content_type: ['image/jpg', 'image/jpeg']
+
+end
