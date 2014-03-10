8e5b369d7f97e2e04cdfdaea92c39afee0b2f85b
pyemkey <py.emkey@gmail.com>
2014-03-10 14:28:27 +0100

    Create nested routes between listings and orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ShopDemo::Application.routes.draw do
   devise_for :users
   root "listings#index"
   resources :listings do
-    resources :order
+    resources :orders
   end
   get '/seller', to: "users#show", as: 'seller'
 
