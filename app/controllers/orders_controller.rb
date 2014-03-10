57e750bcd6c1049fb2cebbe1c2dd68c568720861
pyemkey <py.emkey@gmail.com>
2014-03-10 14:25:52 +0100

    Add new and create action for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,22 @@
 class OrdersController < ApplicationController
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @order = Order.new
+  end
 
+  def create
+    listing = Listing.find(params[:listing_id])
+    @order = listing.orders.build(secure_params)
+    if @order.save
+      redirect_to @order
+    else
+      render :new
+    end
+  end
 
+  private
+
+    def secure_params
+      params.require(:order).permit(:city, :address, :state)
+    end
 end
