c7c0d5170064f381519e1a8b03bbafdd514d553c
pyemkey <py.emkey@gmail.com>
2014-03-13 15:48:13 +0100

    Use the law of demeter, model should talk only to immediate association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,18 @@
 class Order < ActiveRecord::Base
   belongs_to :listing
   belongs_to :user
+
+  delegate :name,
+           :price,
+           :thumb,
+           to: :listing,
+           prefix: :product
+
+  def user_name
+    user.name
+  end
+
+  def sold_date
+    created_at.strftime("%B %d, %Y")
+  end
 end
