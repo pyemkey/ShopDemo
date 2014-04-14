32904e73d03c907bfb5c82ab94db33384b69cd08
pyemkey <py.emkey@gmail.com>
2014-04-14 14:52:16 +0200

    Create seeds for user and listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+11.times do |index|
+  user = User.create(email: Faker::Internet.email,
+                   password: "test1234",
+                   name: Faker::Internet.user_name)
+  l = Listing.create(name: "loker",
+               description: Faker::Lorem.paragraphs(1).join(" "),
+                price: rand(1..268),
+                listing_image: File.open(Rails.root.join("public/assets/#{index+1}.jpg")),
+                user: user)
+end
+
+
