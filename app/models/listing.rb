7dab3dcdc90cbc210ac6e20e9b1275d9b59c1894
pyemkey <py.emkey@gmail.com>
2014-04-14 15:01:17 +0200

    Add image/png format

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id                         :integer          not null, primary key
+#  name                       :string(255)
+#  description                :text
+#  price                      :decimal(8, 2)
+#  listing_image_file_name    :string(255)
+#  listing_image_content_type :string(255)
+#  listing_image_file_size    :integer
+#  listing_image_updated_at   :datetime
+#  user_id                    :integer
+#  created_at                 :datetime
+#  updated_at                 :datetime
+#
+
 class Listing < ActiveRecord::Base
 	has_attached_file :listing_image, 
     styles: { medium: "200x200>", thumb: "100x100"},
@@ -9,7 +26,7 @@ class Listing < ActiveRecord::Base
 	validates_presence_of :name, :description, :price, :listing_image
 	validates :description, length: { maximum: 255 }
 	validates :price, numericality: true
-  validates_attachment_content_type :listing_image, content_type: ['image/jpg', 'image/jpeg']
+  validates_attachment_content_type :listing_image, content_type: ['image/jpg', 'image/jpeg','image/png']
 
   def thumb
     listing_image.url(:thumb)
