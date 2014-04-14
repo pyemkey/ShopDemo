ca623499f3a662bba90e27d754dca1b7f21c3b8a
pyemkey <py.emkey@gmail.com>
2014-04-14 15:09:58 +0200

    Use postgres as default

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140312082851) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: true do |t|
     t.string   "name"
     t.text     "description"
@@ -36,8 +39,8 @@ ActiveRecord::Schema.define(version: 20140312082851) do
     t.datetime "updated_at"
   end
 
-  add_index "orders", ["listing_id"], name: "index_orders_on_listing_id"
-  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
+  add_index "orders", ["listing_id"], name: "index_orders_on_listing_id", using: :btree
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -55,7 +58,7 @@ ActiveRecord::Schema.define(version: 20140312082851) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
