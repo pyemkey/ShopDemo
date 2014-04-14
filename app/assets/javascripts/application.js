f5e49fda8abafe3aa4e50ce84746ffbdda1e2100
pyemkey <py.emkey@gmail.com>
2014-04-14 14:56:31 +0200

    Fix masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,9 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require masonry/jquery.masonry
+//= require masonry/jquery.event-drag
+//= require masonry/jquery.imagesloaded.min
+//= require masonry/jquery.infinitescroll.min
+//= require masonry/modernizr-transitions
+//= require modernizr
 //= require_tree .
--- app/assets/javascripts/mas.js
+++ app/assets/javascripts/mas.js
@@ -1,8 +1,21 @@
-var $container = $('#items');
-    $container.imagesLoaded(function(){
-    $container.masonry({
-      itemSelector : '.item',
-      columnWidth : 290,
-      isAnimated: true
-    });
+// var $container = $('#items');
+//     $container.imagesLoaded(function(){
+//     $container.masonry({
+//       itemSelector : '.item',
+//       columnWidth : 290,
+//       gutterWidth: 40,
+//       isAnimated: true,
+//       isRTL: true
+//     });
+//   });
+
+$(function(){
+
+  $('#items').masonry({
+    itemSelector: '.item',
+    columnWidth: 50,
+    isAnimated: !Modernizr.csstransitions,
+    isRTL: true
   });
+
+});
